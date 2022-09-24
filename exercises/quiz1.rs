6e0914e18d40722c49ae4c3978b2bc0e89c4f615
Marquis Kurt <software@marquiskurt.net>
2022-09-24 12:22:50 -0400

    Start exercises
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(prod: i32) -> i32 {
+    if prod <= 40 { return prod * 2; }
+    return prod;
+}
 
 // Don't modify this function!
 #[test]
